043d7b8fd6a4c0f5986eea04598b2f3aaac2ad31
EC2 Default User <ec2-user@ip-172-31-46-78.ap-northeast-1.compute.internal>
2022-11-01 07:17:10 +0000

    応用課題３復習

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
@@ -24,11 +24,9 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -37,15 +35,14 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path, notice: "successfully delete book!"
+    redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title, :body)
   end
 
   def ensure_correct_user
